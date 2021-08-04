8e9e82eab4e325f42d880f16cba055a81d629494
Rishika <rishikamaroo@gmail.com>
2021-08-03 17:15:25 -0700

    dockerize

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,23 @@
+
+# stage 1: base stage
+FROM node:14.15.1-alpine
+
+# create directory for the container
+WORKDIR /app
+
+# copy configs to /app folder
+COPY package*.json ./
+COPY tsconfig.json ./
+COPY eslint.json ./
+
+# copy source code to /app/src folder
+COPY src /app/src
+
+RUN npm install -g typescript
+RUN npm install
 RUN npm ci
+RUN tsc
+
+# start
+EXPOSE 3000
+ENTRYPOINT node /app/dist/app.js
--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ dotenv.config();
  * MONGO DB
  */
 export const MONGO_PORT = process.env.MONGO_PORT || 27017;
-export const MONGO_HOST = process.env.MONGO_HOST || 'localhost';
+export const MONGO_HOST = process.env.MONGO_HOST || 'host.docker.internal';
 export const MONGO_DB_NAME = process.env.MONGO_DB_NAME || 'recipe';
 export const MONGO_CONNECT_URL =
   process.env.MONGO_CONNECT_URL || `mongodb://${MONGO_HOST}:${MONGO_PORT}`;
@@ -18,7 +18,7 @@ export const MONGO_CONNECT_URL =
  * POSTGRES DB
  */
 export const POSTGRES_PORT = process.env.POSTGRES_PORT || 5432;
-export const POSTGRES_HOST = process.env.POSTGRES_HOST || 'localhost';
+export const POSTGRES_HOST = process.env.POSTGRES_HOST || 'host.docker.internal';
 export const POSTGRES_USER = process.env.POSTGRES_USER || 'postgres';
 export const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD || '1234';
 export const POSTGRES_DB_NAME = 'typeormdemo';
